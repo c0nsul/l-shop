afad3170793263c631d3692b504635ff60dafe66
ConsuL <consul.developer@gmail.com>
2021-09-09 20:13:50 +0300

    middleware migrations

---
+++ app/Http/Middleware/CheckIsAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+        if (!$user->isAdmin()){
+            session()->flash('success', 'Welcome back '.$user->name);
+            return redirect()->route('index');
+        }
+        session()->flash('success', 'Welcome back Admin!');
+        return $next($request);
+    }
+}
